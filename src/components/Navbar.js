de41363e86daac56f9757ef61e4bf544f75da184
Anaro Karo <44401026+blendebox-karo@users.noreply.github.com>
2024-05-06 00:50:53 +0100

    added link to the brand name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,9 +10,15 @@ export default function Navbar() {
 							gap-5 justify-between w-full 
 							items-center max-w-5xl mx-auto"
       >
-        <h3 className="text-3xl text-cyan-500 font-bold">
-          THE<span className="text-cyan-700">_ANARO</span>
-        </h3>
+        <a
+          className="text-sm sm:text-base px-2 lg:px-5 py-2 
+          transition rounded"
+          href="#hero"
+        >
+          <h3 className="text-3xl text-cyan-500 font-bold">
+            THE<span className="text-cyan-700">_ANARO</span>
+          </h3>
+        </a>
         <ul className="flex gap-3 text-cyan-700 font-semibold align-center p-1 flex-wrap">
           <li>
             <a
